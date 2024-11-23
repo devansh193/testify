9644f8657803e082ef9b8021bd888d08526c3694
devansh <devanshverma193@gmail.com>
2024-11-24 02:03:55 +0530

    fix

--- recoil/atom.ts
+++ recoil/atom.ts
@@ -1,75 +1,44 @@
-import { ProductWithQuestions } from "@/action/product";
 import { atom } from "recoil";
 
-interface Question {
-  id: number;
-  text: string;
+export interface FormData {
+  title: string;
+  description: string;
+  image: {
+    file: File | null;
+    preview: string;
+    uploading: boolean;
+    error: string;
+  };
+  questions: string[];
+  emojiRatings: boolean;
+  videoReview: boolean;
+  thankYouMessage: string;
 }
-export const nameAtom = atom<string>({
-  key: "nameAtom",
-  default: "",
+
+export const formDataState = atom<FormData>({
+  key: "formDataState",
+  default: {
+    title: "",
+    description: "",
+    image: {
+      file: null,
+      preview: "",
+      uploading: false,
+      error: "",
+    },
+    questions: [""],
+    emojiRatings: true,
+    videoReview: true,
+    thankYouMessage: "",
+  },
 });
 
-export const emailAtom = atom<string>({
-  key: "emailAtom",
-  default: "",
-});
-
-export const imageAtom = atom<File | null>({
-  key: "imageAtom",
-  default: null,
-});
-
-export const titleAtom = atom<string>({
-  key: "titleAtom",
-  default: "Your Product Name",
-});
-
-export const descriptionAtom = atom<string>({
-  key: "descriptionAtom",
-  default: "Describe your product or service here",
-});
-
-export const questionsAtom = atom<Question[]>({
-  key: "questionsAtom",
-  default: [{ id: 1, text: "How would you rate our product?" }],
-});
-
-export const showLogoAtom = atom<boolean>({
-  key: "showLogoAtom",
-  default: true,
-});
-
-export const logoUrlAtom = atom<string>({
-  key: "logoUrlAtom",
-  default: "/placeholder.svg?height=80&width=80",
-});
-
-export const productAtom = atom<ProductWithQuestions | null>({
-  key: "productAtom",
-  default: null,
-});
-
-export const dialogAtom = atom<boolean>({
-  key: "isDialogOpen",
-  default: false,
-});
-
-export const ratingsAtom = atom({
-  key: "ratingsAtom",
+export const currentStepState = atom<number>({
+  key: "currentStepState",
   default: 0,
 });
 
-export const textReviewAtom = atom<string>({
-  key: "textReviewAtom",
-  default: "",
-});
-
-export const productId = atom<string>({
-  key: "idAtom",
-  default: "",
-});
-export const sidebarAtom = atom<boolean>({
-  key: "sidebarAtom",
+export const isSidebarOpenState = atom<boolean>({
+  key: "isSidebarOpenState",
   default: false,
 });
